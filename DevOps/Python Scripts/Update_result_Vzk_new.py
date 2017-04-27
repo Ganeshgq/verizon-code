e17839a0b0c4867cb24dcfe72fd901fb04cbec9a
ganesh <ganesh@gq.com>
2017-04-27 20:02:48 +0530

    Initial commit

---
+++ DevOps/Python Scripts/Update_result_Vzk_new.py
@@ -0,0 +1,275 @@
+import xml.etree.ElementTree as ET
+import re
+import os
+import sys
+import subprocess
+from urllib2 import Request, urlopen
+from urllib import urlencode
+from json import load, dumps
+from base64 import b64encode
+from xml.etree.ElementTree import Element, SubElement, tostring, ElementTree
+import xml.dom.minidom as minidom
+
+result_file = "testng-results.xml"
+project_name = "EWDV_VZKnowledge"
+en_user = "U1VCUlJBOTpNYXRyaXhAODU="
+baseURL = 'http://onejira-test.verizon.com'
+projectURL = baseURL + '/rest/zapi/latest/util/project-list'
+getCyclesURL = baseURL + '/rest/zapi/latest/cycle'
+getExecutionsURL = baseURL + '/rest/zapi/latest/execution'
+postExecutionsURL = baseURL + '/rest/zapi/latest/execution'
+getissueURL = baseURL + '/rest/api/2/issue'
+searchURL = baseURL + '/rest/api/2/search'
+issueURL = baseURL + '/rest/api/2/issue'
+
+def api_get(restURL, callName):
+    req = Request(restURL)
+    req.add_header('Content-Type', 'application/json')
+    req.add_header('Authorization', 'Basic '+en_user)
+
+    try:
+        res = urlopen(req)
+        code = res.getcode()
+        jsonresp = load(res)
+        #print ("Status "+str(code)+" \n")
+    except:
+        print ("Error executing " +  callName + " API call")
+        sys.exit(1)
+    return jsonresp
+
+def api_post(restURL, postData, callName):
+    values = dumps(postData)
+    req = Request(restURL,values)
+    req.add_header('Content-Type', 'application/json')
+    req.add_header('Authorization', 'Basic '+en_user)
+
+    try:       
+       res = urlopen(req)
+       code = res.getcode()
+       jsonresp = load(res)
+       #print ("Status "+str(code)+" \n")
+    except:
+        print ("Error executing " +  callName + " API call")
+        sys.exit(1)
+    return jsonresp
+
+print ("Finding Automation Results from testng result file...")
+
+result_methods={}
+failure_string={}
+tree=ET.parse(result_file)
+root=tree.getroot()
+print (root)
+for test in root.getiterator('test'):
+   result_methods[test.attrib['name']]=[]
+   for cla in test.getiterator('class'):
+      for meth in cla.getiterator('test-method'):
+         result_methods[test.attrib['name']].append(meth.attrib['status'])
+         if meth.attrib['status'] == "FAIL":
+             failure_string[test.attrib['name']] = []
+             for excep in meth.getiterator('exception'):
+                 failure_string[test.attrib['name']].append(excep[0].text)
+
+print (result_methods)
+print (failure_string)
+
+result={}
+failed_tests=[]
+for i in result_methods:
+   if 'FAIL' in result_methods[i]:
+     result[re.sub('_','-',i)]=2
+     failed_tests.append(i)
+   elif 'SKIP' in result_methods[i]:
+     result[re.sub('_','-',i)]=-1
+   else:
+     result[re.sub('_','-',i)]=1
+     
+
+print (result)
+
+print ("Results File Parsed! Execution Status Saved!\n")
+
+print ("Finding Project ID...")
+response_project = api_get(projectURL, "Project")
+
+for i in response_project["options"]:
+    if i['label'] == project_name:
+        projectId=i['value']
+print (projectId)
+
+print ("Finding Cycle ID...")
+cycle_name = sys.argv[1]
+cycleParams = {
+     "projectId": projectId
+}
+cycleURL = getCyclesURL + "?" + urlencode(cycleParams)
+response_cycle = api_get(cycleURL, "Cycle")
+
+for i in response_cycle["-1"]:
+    for key in i:
+        if isinstance(i[key],dict) and i[key]["name"] == cycle_name:
+            cycleId = key
+print (cycleId)
+
+print ("Grabbing Executions from Fetched Cycle...")
+
+fetchExecutionParams = {
+     "action": "expand",
+     "cycleId": cycleId
+}
+
+getExecutionsURL = getExecutionsURL + '/?' + urlencode(fetchExecutionParams)
+response_execution = api_get(getExecutionsURL, "Execution")
+
+fetchedExecutions = response_execution['executions']
+executionIdList = {}
+for rs in fetchedExecutions:
+   executionIdList[rs['issueKey']]=str(rs['id'])
+
+print ("Fetched Executions Completed. Number of Execution Assignments Found: " + str(len(executionIdList)) + "\n")
+print (executionIdList)
+
+# ///// Quick Execute Based on Fetched Executions ///// # Quick execute based on the parsed results file earlier
+
+print ("Updating Cycle Execution(s) Status with Automation Results...")
+
+for x in executionIdList:
+#   print (x)
+   if x in result:
+      status_data = {"status": result[x]}
+      executeURL = postExecutionsURL + '/' + executionIdList[x] + '/quickExecute'
+      api_post(executeURL, status_data, "Status Execute")
+
+print ("Automation Cycle Updated with Execution Status")
+
+jiracli = "atlassian-cli-4.5.0/jira.sh"
+
+def create_issue(project, summary):
+    create_data = {"fields": {"project":{"id": project},"summary": summary,"issuetype": { "name": "Bug"}}}
+    response_create = api_post(issueURL, create_data, "Create Issue" )
+    return response_create["key"]
+
+def attach_file(issue, file_location):
+    out = subprocess.Popen(['curl', '-X', 'POST', '-H', "X-Atlassian-Token: no-check", '-H', "Authorization:Basic "+en_user, '-F', "file=@"+file_location, issueURL+'/'+issue+'/attachments'],stdout=subprocess.PIPE)
+    return out.stdout.read()
+
+def link_issue(issue1, issue2):
+
+    out = subprocess.Popen([jiracli, '--action', 'linkIssue', '--issue', issue1, '--toIssue', issue2, '--link', 'relates to'],stdout=subprocess.PIPE)
+
+    return out.stdout.read()
+
+def search_issue(project, linked_issue):
+    search_url = issueURL+"/"+linked_issue+"?fields=issuelinks"
+    response_search = api_get(search_url, "Search")
+    linked_issues=[]
+    if len(response_search["fields"]["issuelinks"]) > 0:
+        for i in response_search["fields"]["issuelinks"]:
+            try:
+                linked_issues.append(i["outwardIssue"]["key"])
+            except:
+                linked_issues.append(i["inwardIssue"]["key"])
+    else:
+        return linked_issues
+    open_issues=[]
+    for issue in linked_issues:
+        status_url = issueURL+"/"+issue+"?fields=status"
+        response_status = api_get(status_url, "Status")
+        if response_status["fields"]["status"]["name"] != "Closed":
+            open_issues.append(issue)
+    return open_issues
+                
+def update_issue(issue, comment):
+    updateURL = issueURL+"/"+issue+"/comment"
+    print (updateURL)
+    update_data = {"body": comment}
+    print (update_data)
+    response_update = api_post(updateURL, update_data, "Update Issue" )
+    return response_update 
+ 
+def close_issue(issue):
+
+    out = subprocess.Popen([jiracli, '--action', 'transitionIssue', '--issue', issue, '--transition', "Close Issue", '--comment', "Closing the issue as the test case passed on test cycle "+cycle_name],stdout=subprocess.PIPE)
+
+    return out.stdout.read()
+
+issues_created=[]
+issues_updated=[]
+issues_closed=[]
+
+for i in result:
+
+    if result[i] == 1:
+
+        cli_out = search_issue(project_name, i)
+
+        print (cli_out)
+
+        if len(cli_out) != 0:
+
+            for j in cli_out:
+                print j
+                print (close_issue(j))
+                issues_closed.append(j)
+
+    elif result[i] == 2:
+
+        cli_out = search_issue(project_name, i)
+
+        print (cli_out)
+
+        if len(cli_out) != 0:
+
+            for j in cli_out:
+                update_issue(j, failure_string[i][0].split("\n")[1] )
+                issues_updated.append(j)
+                file_out = os.popen("ls  VzKnowledge/reports/ScreenShots/*"+i+"*.png")
+                file_name = file_out.read().split('\n')[0]
+                if file_name != '':
+                    attach_file(j, file_name)
+
+        else:
+
+            cli_out = create_issue(projectId, failure_string[i][0].split("\n")[1] )
+            issues_created.append(cli_out)
+
+            file_out = os.popen("ls  VzKnowledge/reports/ScreenShots/*"+i+"*.png")
+
+            file_name = file_out.read().split('\n')[0]
+            if file_name != '':
+                attach_file(cli_out, file_name)
+            link_issue(i,cli_out)
+                    
+
+f=open('testng-results.xml')
+lines=f.readlines()
+result = lines[1].split(" ")
+
+email_file = open("email.txt",'w')
+email_file.write("================================================\n")
+email_file.write("Total number of test cases executed "+result[3]+"\n")
+email_file.write("No of test cases "+result[4].split('>')[0]+"\n")
+email_file.write("No of test cases "+result[2]+"\n")
+email_file.write("No of test cases "+result[1]+"\n")
+email_file.write("================================================\n")
+email_file.write("Number of issue created = "+str(len(issues_created))+"\n")
+email_file.write(str(issues_created)+"\n")
+email_file.write("Number of issue updated = "+str(len(issues_updated))+"\n")
+email_file.write(str(issues_updated)+"\n")
+email_file.write("Number of issue closed = "+str(len(issues_closed))+"\n")
+email_file.write(str(issues_closed)+"\n")
+email_file.write("================================================\n")
+email_file.close()
+
+print ("Total number of test cases executed "+result[3])
+print ("No of test cases "+result[4].split('>')[0])
+print ("No of test cases "+result[2])
+print ("No of test cases "+result[1])
+print ("================================================")
+print ("Number of issue created = "+str(len(issues_created)))
+print (str(issues_created))
+print ("Number of issue updated = "+str(len(issues_updated)))
+print (str(issues_updated))
+print ("Number of issue closed = "+str(len(issues_closed)))
+print (str(issues_closed))
+
---
+++ DevOps/Python Scripts/xml_generator_Prism.py
@@ -0,0 +1,188 @@
+import os
+import re
+import sys
+import time
+from json import load, dumps
+from urllib2 import Request, urlopen
+from urllib import urlencode
+from xml.etree.ElementTree import Element, SubElement, tostring, ElementTree
+import xml.dom.minidom as minidom
+
+project_name = "C3OV_PRISM"
+if sys.argv[1] == "None":
+    cycle_name="Regression-Auto-"+time.strftime("%Y/%m/%d-%H:%M:%S")
+else:
+    cycle_name=sys.argv[1]
+property_file = open("env.properties",'w')
+property_file.write("cycle_name="+cycle_name)
+property_file.close()
+en_user = "U1VCUlJBOTpNYXRyaXhAODU="
+baseURL = 'http://onejira-test.verizon.com'
+projectURL = baseURL + '/rest/zapi/latest/util/project-list'
+cycleURL = baseURL + '/rest/zapi/latest/cycle'
+execURL = baseURL + '/rest/zapi/latest/execution'
+issuesURL = baseURL + '/rest/zapi/latest/issues'
+testtocycleURL = baseURL + '/rest/zapi/latest/execution/addTestsToCycle'
+issueURL = baseURL + '/rest/api/2/issue'
+searchURL = baseURL + '/rest/api/2/search'
+
+def api_get(restURL, callName):
+    req = Request(restURL)
+    req.add_header('Content-Type', 'application/json')
+    req.add_header('Authorization', 'Basic '+en_user)
+
+    try:
+        res = urlopen(req)
+        code = res.getcode()
+        jsonresp = load(res)
+    except:
+        print ("Error executing " +  callName + " API call")
+        sys.exit(1)
+    return jsonresp
+
+def api_post(restURL, postData, callName):
+    values = dumps(postData)
+    req = Request(restURL,values)
+    req.add_header('Content-Type', 'application/json')
+    req.add_header('Authorization', 'Basic '+en_user)
+
+    try:
+        res = urlopen(req)
+        code = res.getcode()
+        jsonresp = load(res)
+        print ("Status "+str(code)+" \n")
+    except:
+        print ("Error executing " +  callName + " API call")
+        sys.exit(1)
+    return jsonresp
+
+def f(x):
+    if "-" in x:
+	    num=x.split("-",2)[1]
+    elif "_" in x:
+	    num=x.split("_",2)[1]
+    if num.isdigit():
+        return int(num)
+    return x
+
+print ("Finding Project ID...")
+response_project = api_get(projectURL, "Project")
+
+for i in response_project["options"]:
+    if i['label'] == project_name:
+        projectId=i['value']
+        print (projectId)
+
+if sys.argv[1] == "None":
+    print ("Creating Test Cycle in Jira...")
+    cycle_data = {"name":cycle_name,"projectId":projectId,"versionId":"-1"}
+    response_cycle = api_post(cycleURL, cycle_data, "Cycle")
+    cycleId = response_cycle['id']
+    print (cycleId)
+
+    print ("Finding Test cases in the project...")
+    test_names=[]
+    class_names=[]
+    search_data = {"jql":"project = "+project_name+" & type = Test", "maxResults":250, "fields":["id","key","status"]}
+    response_search = api_post(searchURL, search_data, "Search")
+    for issue in (response_search['issues']):
+        if issue["fields"]["status"]["name"] != "Closed":
+            test_names.append(issue['key'])
+            class_names.append(re.sub('-','_',issue['key']))
+	
+
+    test_names = sorted(set(test_names), key=f)
+    class_names = sorted(set(class_names), key=f)
+
+
+    print (test_names)
+    print (class_names)
+
+    print ("Adding Test cases to Test Cycle in Jira...")
+    testtocycle_data = {"issues":test_names,"versionId":-1,"cycleId":cycleId,"projectId":projectId}
+    response_testtocycle = api_post(testtocycleURL, testtocycle_data, "Add test to cycle")
+    print (response_testtocycle)
+
+else:
+    print ("Finding Cycle ID...")
+    cycleValues = {
+     "projectId": projectId
+     }
+    cycleURL = cycleURL + "?" + urlencode(cycleValues)
+    response_cycle = api_get(cycleURL, "Cycle")
+
+    for i in response_cycle["-1"]:
+        for key in i:
+            if isinstance(i[key],dict) and i[key]["name"] == cycle_name:
+                cycleId = key
+                print (cycleId)
+
+    print ("Finding Test cases...")
+    test_names=[]
+    class_names=[]
+    execValues = {
+     "cycleId": cycleId
+     }
+    execURL = execURL + "?" + urlencode(execValues)
+    response_exec = api_get(execURL, "Execution")
+
+    for i in range(len(response_exec["executions"])):
+        issue=response_exec["executions"][i]["issueKey"]
+        test_names.append(issue)
+        class_names.append(re.sub('-','_',issue))
+
+    test_names.reverse()
+    class_names.reverse()
+    print (test_names)
+    print (class_names)
+
+print ("Finding Class path from test case label...")
+test_labels={}
+for test in test_names:
+    loopURL = issueURL + "/" + test
+    response_issue = api_get(loopURL, "Issue")
+    test_labels[test] = response_issue["fields"]["labels"]
+print (test_labels)
+
+print ("Generating testng xml file...")
+root = Element('suite', name=cycle_name, parallel="tests", threadcount="1")
+child1 = SubElement(root, "listeners")
+c1 = SubElement(child1, 'listener', classname="utility.PrismListener")
+for i in range(len(test_names)):
+    child = SubElement(root, "test", name=test_names[i])
+    node1_1 = SubElement(child, "parameter", name="testCaseId", value=test_names[i])
+    node1_2 = SubElement(child, "parameter", name="BrowserType", value="IE")
+    node1_3 = SubElement(child, "parameter", name="BrowserType1", value="firefox")
+    node2 = SubElement(child, "classes")
+    node3 = SubElement(node2, 'class', name="prism.testcases."+class_names[i])
+    '''
+    if test_labels[test] != []:
+        node3 = SubElement(node2, 'class', name="prism.testcases."+class_names[i])
+    else:
+        print ("Found a test case without label in JIRA. Please add class name as label.")
+        sys.exit(1)
+    '''
+
+rough_string = tostring(root, 'utf-8')
+reparsed = minidom.parseString(rough_string)
+pretty_out = reparsed.toprettyxml(indent="\t")
+
+with open("first.xml", "w") as f1:
+    f1.write(pretty_out)
+f1.close()
+
+with open("first.xml") as f2:
+    with open("testng.xml", "w") as f3:
+        for line in f2:
+            if "classname" in line:
+                output_line = line.replace("classname", "class-name")
+            elif "threadcount" in line:
+                output_line = line.replace("threadcount", "thread-count")
+            else:
+                output_line = line
+            print (output_line)
+            f3.write(output_line)
+
+f2.close()
+f3.close()
+
